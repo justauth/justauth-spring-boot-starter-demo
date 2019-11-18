93d43e44a924283d554226c58261ce246db789df
Yangkai.Shen <237497819@qq.com>
2019-11-18 23:20:37 +0800

    :sparkles: 自定义钉钉网页授权测试

--- src/main/java/com/xkcoding/justauthspringbootstarterdemo/extend/ExtendSource.java
+++ src/main/java/com/xkcoding/justauthspringbootstarterdemo/extend/ExtendSource.java
@@ -11,6 +11,40 @@ import me.zhyd.oauth.config.AuthSource;
  * @date Created in 2019/10/9 14:14
  */
 public enum ExtendSource implements AuthSource {
+    /**
+     * 企业微信
+     */
+    WEB_ENTERPRISE_WE_CHAT {
+        /**
+         * 授权的api
+         *
+         * @return url
+         */
+        @Override
+        public String authorize() {
+            return "https://open.weixin.qq.com/connect/oauth2/authorize";
+        }
+
+        /**
+         * 获取accessToken的api
+         *
+         * @return url
+         */
+        @Override
+        public String accessToken() {
+            return "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+        }
+
+        /**
+         * 获取用户信息的api
+         *
+         * @return url
+         */
+        @Override
+        public String userInfo() {
+            return "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo";
+        }
+    },
 
     /**
      * 测试
