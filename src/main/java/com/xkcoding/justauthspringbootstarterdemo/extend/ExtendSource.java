3b231a6d07428fa6d2c95fcae68a8471f2478ba7
yadong.zhang <yadong.zhang0415@gmail.com>
2020-08-26 10:15:01 +0800

    :arrow_up: 升级 justauth-spring-boot-starter 到 1.3.4 版

--- src/main/java/com/xkcoding/justauthspringbootstarterdemo/extend/ExtendSource.java
+++ src/main/java/com/xkcoding/justauthspringbootstarterdemo/extend/ExtendSource.java
@@ -99,5 +99,40 @@ public enum ExtendSource implements AuthSource {
         public String refresh() {
             return null;
         }
+    },
+
+    /**
+     * 自己搭建的gitlab私服
+     */
+    MYGITLAB {
+        /**
+         * 授权的api
+         *
+         * @return url
+         */
+        @Override
+        public String authorize() {
+            return "http://gitlab.innodev.cn:9001/oauth/authorize";
+        }
+
+        /**
+         * 获取accessToken的api
+         *
+         * @return url
+         */
+        @Override
+        public String accessToken() {
+            return "http://gitlab.innodev.cn:9001/oauth/token";
+        }
+
+        /**
+         * 获取用户信息的api
+         *
+         * @return url
+         */
+        @Override
+        public String userInfo() {
+            return "http://gitlab.innodev.cn:9001/api/v4/user";
+        }
     }
 }
